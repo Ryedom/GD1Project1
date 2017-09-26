e7421ef433d648c4a95eba596ff20a58d4acb623
lessuknow <chenc18@rpi.edu>
2017-09-25 21:24:22 -0400

    Added a new cutscene.

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -4,6 +4,8 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.FlxG;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
 
 class MenuState extends FlxState
 {
@@ -20,17 +22,18 @@ class MenuState extends FlxState
 		_bg.setBg("assets/images/assets/ge_background.png");
 		add(_bg);
 		
+		_banner = new FlxButton(40, 40, "", clickPlay);
+		_banner.loadGraphic("assets/images/baller_banner.png");
+		_banner.updateHitbox();
+		_banner.setPosition((FlxG.width/2 - _banner.width/2), -_banner.height);
+		FlxTween.tween(_banner, {x : (FlxG.width / 2 - _banner.width / 2), y : (FlxG.height * 1.35 / 4 - _banner.height / 2)}, 1, {ease: FlxEase.cubeIn});
+				
 		_playButton = new FlxButton(40, 40, "", clickPlay);
 		//_playButton.setGraphicSize(400, 200);
 		_playButton.loadGraphic("assets/images/COOLbuttoNASSET.png");
 		_playButton.updateHitbox();
-		_playButton.setPosition(FlxG.width / 2 - _playButton.width/2, FlxG.height*3 / 4 - _playButton.height/2);
-		
-		_banner = new FlxButton(40, 40, "", clickPlay);
-		_banner.loadGraphic("assets/images/baller_banner.png");
-		_banner.updateHitbox();
-		_banner.setPosition(FlxG.width / 2 - _banner.width/2, FlxG.height*1.35 / 4 - _banner.height/2);
-				
+		_playButton.setPosition((FlxG.width/2 - _playButton.width/2), -_playButton.height);
+		FlxTween.tween(_playButton, {x : (FlxG.width / 2 - _playButton.width / 2), y : (FlxG.height*3 / 4 - _playButton.height/2)}, 1, {ease: FlxEase.cubeIn});
 		
 		add(_banner);
 		add(_playButton);
--- source/SceneFour.hx
+++ source/SceneFour.hx
@@ -4,10 +4,8 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.math.FlxRandom;
 import flixel.util.FlxTimer;
-import flixel.FlxG;
-import flixel.addons.transition;
 
-class SceneOne extends FlxState
+class SceneFour extends FlxState
 {
 	var _bg:BackgroundImage;
 	
@@ -25,47 +23,36 @@ class SceneOne extends FlxState
 		timer.start(_timerDelay);
 		
 		_bg = new BackgroundImage();
-		_bg.setBg("assets/images/CutsceneAssets/kitchen_bkgrnd.png");
+		_bg.setBg("assets/images/CutsceneAssets/letter_bg.jpg");
 		add(_bg);
 		
 		_char = new Array();
-		
+
 		_char[0] = new EventImage();
-		_char[0].setImage("assets/images/CutsceneAssets/kitchen_fridge_new.png", 1280, 720);
+		_char[0].setImage("assets/images/CutsceneAssets/paper.png", 1280, 720);
 		_char[0].setPosition(0, 0);
 		_char[0].changeFloorHeight(0);
-		
+
 		_char[1] = new EventImage();
-		_char[1].setImage("assets/images/CutsceneAssets/kitchen_mom_new.png", 1280, 720);
+		_char[1].setImage("assets/images/CutsceneAssets/l_hand.png", 1280, 720);
 		_char[1].setPosition(0, 0);
 		_char[1].changeFloorHeight(0);
-		
+
 		_char[2] = new EventImage();
-		_char[2].setImage("assets/images/CutsceneAssets/kitchen_bang_new.png", 1280, 720);
+		_char[2].setImage("assets/images/CutsceneAssets/r_hand.png", 1280, 720);
 		_char[2].setPosition(0, 0);
 		_char[2].changeFloorHeight(0);
-		
+
 		_char[3] = new EventImage();
-		_char[3].setImage("assets/images/CutsceneAssets/kitchen_son_new.png", 1280, 720);
+		_char[3].setImage("assets/images/CutsceneAssets/h_binary.png", 1280, 720);
 		_char[3].setPosition(0, 0);
 		_char[3].changeFloorHeight(0);
-		
-	
+
 		_char[4] = new EventImage();
-		_char[4].setImage("assets/images/CutsceneAssets/kitchen_daughter_new.png", 1280, 720);
+		_char[4].setImage("assets/images/CutsceneAssets/sig.png", 1280, 720);
 		_char[4].setPosition(0, 0);
-		_char[4].changeFloorHeight(0);		
-		
-		_char[5] = new EventImage();
-		_char[5].setImage("assets/images/CutsceneAssets/kitchen_table_new.png", 1280, 720);
-		_char[5].setPosition(0, 0);
-		_char[5].changeFloorHeight(0);		
-		
-		_char[6] = new EventImage();
-		_char[6].setImage("assets/images/CutsceneAssets/kitchen_dad_new.png", 1280, 720);
-		_char[6].setPosition(0,0);
-		_char[6].changeFloorHeight(0);	
-		
+		_char[4].changeFloorHeight(0);
+
 		_totNum = _char.length;
 		_curNum = 0;
 	}
@@ -78,10 +65,6 @@ class SceneOne extends FlxState
 			add(_char[_curNum]);
 			_curNum++;
 			timer.start(_timerDelay);
-		}
-		if (FlxG.keys.pressed.UP)
-		{
-			FlxG.switchState(new SceneTwo());
 		}
 	}
 }
